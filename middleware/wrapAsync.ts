5ffc8e480821ae8019ed0aff0a8815e6351b86ff
Mansuri Ayan <2004ayanmansuri@gmail.com>
2025-08-30 21:08:40 +0530

    Added wrapAsync asyncronious error handler

--- middleware/wrapAsync.ts
+++ middleware/wrapAsync.ts
@@ -1,8 +1,12 @@
-/** @format */
-
-import { NextFunction, Request, Response } from "express";
+import { Request, Response, NextFunction } from "express";
 
 export const wrapAsync =
-  (fn: any) => (err: any, req: Request, res: Response, next: NextFunction) => {
-    Promise.resolve(fn(req, res, next).catch(next));
+  (fn: (req: Request, res: Response, next: NextFunction) => Promise<any>) =>
+  (req: Request, res: Response, next: NextFunction) => {
+    Promise.resolve(fn(req, res, next)).catch((err) => {
+      console.error("❌ Async error caught in wrapAsync:", err);
+      next(err);
+    });
   };
+
+  
--- routes/user.route.ts
+++ routes/user.route.ts
@@ -4,8 +4,5 @@ import express, { Request, Response } from "express";
 import { registrationUser } from "../controllers/user.controllers";
 import { wrapAsync } from "../middleware/wrapAsync";
 const userRouter = express.Router({ mergeParams: true });
-// userRouter.post("/registration", async (req: Request, res: Response) => {
-//   return res.send("working");
-// });
-userRouter.post("/registration", registrationUser);
+userRouter.post("/registration", wrapAsync(registrationUser));
 export default userRouter;
